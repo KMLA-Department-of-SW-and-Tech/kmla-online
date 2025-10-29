d24e67a7a7a467311f644d078f1af4ffbb763384
jigangjon <jigangjon@gmail.com>
2025-10-29 09:29:26 +0900

    removed unused import from routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,4 @@
-import {
-  type RouteConfig,
-  index,
-  layout,
-  route,
-} from "@react-router/dev/routes";
+import { type RouteConfig, index, layout } from "@react-router/dev/routes";
 
 export default [
   layout("layouts/layout-main.tsx", [index("routes/home.tsx")]),
